10fa9c11b53549bc23c0c4f5cec06ac4c97c491a
SeraphyBR <luisjuniorbr@gmail.com>
2022-03-17 09:55:36 -0300

    Simplificando execução do servidor local, adicionado opção para criar o banco sqlite se não existir

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,7 @@ mod server;
 fn main() {
   tauri::Builder::default()
     .setup(|_app| {
-      tauri::async_runtime::spawn(server::start_inside_tauri("0.0.0.0:8080"));
+      tauri::async_runtime::spawn_blocking(|| server::start("0.0.0.0:8080"));
 
       Ok(())
     })
--- src-tauri/src/server/mod.rs
+++ src-tauri/src/server/mod.rs
@@ -1,11 +1,11 @@
 use std::env;
+use std::str::FromStr;
 use std::sync::Arc;
 
 use axum::extract::Extension;
 use dotenv::dotenv;
-use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 use sqlx::SqlitePool;
-use tokio::task::JoinHandle;
 
 mod router;
 
@@ -14,11 +14,16 @@ pub struct ApiContext {
   db: SqlitePool,
 }
 
-pub async fn start_inside_tauri(addr: &'static str) -> JoinHandle<()> {
+#[tokio::main]
+pub async fn start(addr: &'static str) {
   dotenv().ok();
 
+  let db_connection_options = SqliteConnectOptions::from_str(&env::var("DATABASE_URL").unwrap())
+    .unwrap()
+    .create_if_missing(true);
+
   let db = SqlitePoolOptions::new()
-    .connect(&env::var("DATABASE_URL").unwrap())
+    .connect_with(db_connection_options)
     .await
     .unwrap();
 
@@ -27,15 +32,8 @@ pub async fn start_inside_tauri(addr: &'static str) -> JoinHandle<()> {
   let shared_state = Arc::new(ApiContext { db });
   let app = router::api_router().layer(Extension(shared_state));
 
-  let rt = tokio::runtime::Handle::current();
-  let rt_ = rt.clone();
-  rt.spawn_blocking(move || {
-    rt_.block_on(async {
-      let local = tokio::task::LocalSet::new();
-      local
-        .run_until(axum::Server::bind(&addr.parse().unwrap()).serve(app.into_make_service()))
-        .await
-        .unwrap();
-    })
-  })
+  axum::Server::bind(&addr.parse().unwrap())
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }

6d5698e420b1cd4aa96e911cc434a916466d6d3b
SeraphyBR <luisjuniorbr@gmail.com>
2022-03-23 11:26:22 -0300

    Atualizado versão de dependencias, adicionado migration

---
+++ src-tauri/migrations/20220323132339_create_projects_table.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS projects (
+  id TEXT NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL,
+  filename TEXT,
+  fileblob BLOB,
+  deleted INTEGET NOT NULL,
+  created_timestamp INTEGER NOT NULL,
+  updated_timestamp INTEGER
+)

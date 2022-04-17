de692939bccba9b7821e43a04eddef276afff748
SeraphyBR <luisjuniorbr@gmail.com>
2022-04-17 09:57:49 -0300

    Adicionado react-flow-renderer, e componente de teste

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import BasePage from "../../components/basepage/BasePage"
+import Flow from "../../components/flow/Flow"
 import * as S from "./Home.style"
 
 const Home: React.FC = () => {
@@ -7,6 +8,9 @@ const Home: React.FC = () => {
     <BasePage>
       <S.HomeWrapper>
         <p>teste</p>
+        <div style={{ width: "auto", height: "500px" }}>
+          <Flow />
+        </div>
       </S.HomeWrapper>
     </BasePage>
   )

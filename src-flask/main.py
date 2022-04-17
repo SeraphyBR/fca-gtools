422c651831456c6ff92c70ce642f264fae77baef
SeraphyBR <luisjuniorbr@gmail.com>
2022-04-17 08:34:12 -0300

    Atualizando rota de teste no flask, para já rodar todos os calculos de uma vez, posteriormente de acordo com os testes, posso considerar quebrar as operações caso demore muito para processar

--- src-flask/main.py
+++ src-flask/main.py
@@ -4,6 +4,9 @@ from flask import Flask
 from fcatools.triadic.triadic_context import TriadicContext
 from fcatools.dyadic.dyadic_concept import DyadicConcept
 from fcatools.triadic.triadic_incidence import TriadicIncidence
+from fcatools.dyadic.dyadic_lattice import DyadicLattice
+from fcatools.dyadic.dyadic_generator import DyadicGenerator
+from fcatools.triadic.triadic_association_rule import TriadicAssociationRule
 
 app = Flask(__name__)
 
@@ -28,6 +31,32 @@ def fca_tools():
             "dyadic_context.data")
         concepts = DyadicConcept.get_concepts_d_peeler(
             "dyadic_context.data", "dyadic_concepts.data")
+        lattice, _ = DyadicLattice.build_lattice_iPred(concepts)
+
+        generators = lattice.compute_generators()
+
+        dyadic_association_rules = lattice.compute_association_rules(
+            generators)
+
+        bacars_implication_rules, bacars_association_rules = TriadicAssociationRule.calculate_bacars_from_dyadic_rules(
+            dyadic_association_rules, '-')
+        bcaars_implication_rules, bcaars_association_rules = TriadicAssociationRule.calculate_bcaars_from_dyadic_rules(
+            dyadic_association_rules, '-')
+        print('\nBACARS Rules:')
+        print('implications:')
+        for value in bacars_implication_rules:
+            print(value)
+        print('associations:')
+        for value in bacars_association_rules:
+            print(value)
+
+        print('\nBCAARS Rules:')
+        print('implications:')
+        for value in bcaars_implication_rules:
+            print(value)
+        print('associations:')
+        for value in bcaars_association_rules:
+            print(value)
 
         return vars(concepts[0])
     return

d48f15a9088276b6c88101ac4e6ab3d1b1f9fce5
SeraphyBR <luisjuniorbr@gmail.com>
2022-05-05 10:36:15 -0300

    Adicionando a api flask como sidecar do tauri

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::process::Command;
+use tauri_build;
+
+fn build_and_move_python_api_sidecar() {
+  Command::new("python")
+    .arg("../src-flask/build.py")
+    .output()
+    .expect("Error when running src-flask/build.py");
+
+  fs::create_dir_all("binaries").expect("Error on create directory 'binaries'");
+
+  let target = std::env::var("TARGET").unwrap();
+
+  fs::copy("../src-flask/dist/main", "binaries/flask").expect("Could not copy python api binary");
+  fs::copy(
+    "../src-flask/dist/main",
+    format!("binaries/flask-{}", target),
+  )
+  .expect("Could not copy python api binary with target");
+}
+
 fn main() {
+  build_and_move_python_api_sidecar();
   tauri_build::build()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,18 @@
 )]
 
 mod server;
+use tauri::api::process::Command;
 
 fn main() {
   tauri::Builder::default()
     .setup(|_app| {
       tauri::async_runtime::spawn_blocking(|| server::start("0.0.0.0:8080"));
+      tauri::async_runtime::spawn_blocking(|| {
+        Command::new_sidecar("flask")
+          .expect("Failed to create 'flask' binary command")
+          .spawn()
+          .unwrap()
+      });
 
       Ok(())
     })

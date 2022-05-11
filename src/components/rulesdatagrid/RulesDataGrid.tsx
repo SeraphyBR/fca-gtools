b06b8960a15c013a0e51797a76f27f2d1938d2dc
SeraphyBR <luisjuniorbr@gmail.com>
2022-05-11 09:34:35 -0300

    Novo componente data grid para exibição das regras

---
+++ src/components/rulesdatagrid/RulesDataGrid.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { TriadicAssociationRule } from "../../models/data"
+import { DataGrid, GridColDef } from "@mui/x-data-grid"
+import { Box, SxProps, Theme } from "@mui/material"
+
+type RulesDataGridProps = {
+  rules: TriadicAssociationRule[]
+  loading?: boolean
+  sx?: SxProps<Theme>
+}
+
+const RulesDataGrid: React.FC<RulesDataGridProps> = (props) => {
+  const columns: GridColDef[] = [
+    {
+      field: "implication",
+      headerName: "Implicações",
+      headerAlign: "center",
+      align: "center",
+      width: 200
+    },
+    {
+      field: "conditions",
+      headerName: "Condições",
+      headerAlign: "center",
+      align: "center",
+      width: 200
+    },
+    {
+      field: "support",
+      headerName: "Suporte",
+      headerAlign: "center",
+      type: "number",
+      align: "center"
+    },
+    {
+      field: "confidence",
+      headerName: "Confiança",
+      headerAlign: "center",
+      type: "number",
+      align: "center"
+    }
+  ]
+
+  const rows = props.rules.map((r, idx) => ({
+    id: idx,
+    implication: r.left_side.join(", ") + " -> " + r.right_side.join(", "),
+    conditions: r.condition.join(", "),
+    support: r.support,
+    confidence: r.confidence
+  }))
+
+  return (
+    <Box sx={props.sx}>
+      <DataGrid rows={rows} columns={columns} loading={props.loading} disableSelectionOnClick autoPageSize />
+    </Box>
+  )
+}
+
+export default RulesDataGrid
--- src/pages/data/Data.tsx
+++ src/pages/data/Data.tsx
@@ -1,9 +1,10 @@
-import { Divider, Typography } from "@mui/material"
+import { Box, Button, Divider, Typography } from "@mui/material"
 import React, { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import BasePage from "../../components/basepage/BasePage"
+import RulesDataGrid from "../../components/rulesdatagrid/RulesDataGrid"
 import { FcatoolsTriadicData } from "../../models/data"
 import { getWorkingProject } from "../../redux/data/selectors"
 import { getDataFromFcatools } from "../../services/backend"
@@ -32,59 +33,17 @@ const DataPage: React.FC = () => {
     <BasePage>
       <Typography variant="h4">{t("title") + " - " + project?.name}</Typography>
       <Divider sx={{ margin: "8px 0px" }} />
-      <p>{project?.name}</p>
-      {fcaTriadicData !== undefined && (
-        <div>
-          <div>
-            <p>BACARS implication rules:</p>
-            <ol>
-              {fcaTriadicData.bacars_implication_rules.map((r, i) => (
-                <li key={i}>
-                  {"("}
-                  {r.left_side.join(", ")} {"->"} {r.right_side.join(", ")}
-                  {")"} {r.condition.join(", ")} {"(sup: "} {r.support} {", conf: "} {r.confidence} {")"}
-                </li>
-              ))}
-            </ol>
-          </div>
-          <div>
-            <p>BACARS association rules:</p>
-            <ol>
-              {fcaTriadicData.bacars_association_rules.map((r, i) => (
-                <li key={i}>
-                  {"("}
-                  {r.left_side.join(", ")} {"->"} {r.right_side.join(", ")}
-                  {")"} {r.condition.join(", ")} {"(sup: "} {r.support} {", conf: "} {r.confidence} {")"}
-                </li>
-              ))}
-            </ol>
-          </div>
-          <div>
-            <p>BCAARS implication rules:</p>
-            <ol>
-              {fcaTriadicData.bcaars_implication_rules.map((r, i) => (
-                <li key={i}>
-                  {"("}
-                  {r.left_side.join(", ")} {"->"} {r.right_side.join(", ")}
-                  {")"} {r.condition.join(", ")} {"(sup: "} {r.support} {", conf: "} {r.confidence} {")"}
-                </li>
-              ))}
-            </ol>
-          </div>
-          <div>
-            <p>BCAARS association rules:</p>
-            <ol>
-              {fcaTriadicData.bcaars_association_rules.map((r, i) => (
-                <li key={i}>
-                  {"("}
-                  {r.left_side.join(", ")} {"->"} {r.right_side.join(", ")}
-                  {")"} {r.condition.join(", ")} {"(sup: "} {r.support} {", conf: "} {r.confidence} {")"}
-                </li>
-              ))}
-            </ol>
-          </div>
-        </div>
-      )}
+      <Box mb="24px" width="100%" height="36px" alignItems="center" display="inline-flex" gap="12px">
+        <Button variant="outlined">Gerar regras BACARS</Button>
+        <Button variant="outlined">Gerar regras BCAARS</Button>
+      </Box>
+      <Box>
+        <RulesDataGrid
+          rules={fcaTriadicData ? fcaTriadicData.bacars_implication_rules : []}
+          loading={fcaTriadicData === undefined}
+          sx={{ height: "calc(100vh - 200px)" }}
+        />
+      </Box>
     </BasePage>
   )
 }
